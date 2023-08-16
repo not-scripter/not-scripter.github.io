2936a96322b3c3bb02b411c3e3b931caf9956c7d
Mrinmoy <xmrinmoy@proton.me>
2023-08-16 19:33:49 +0530

    Add files via upload

---
+++ assets/script/header.js
@@ -0,0 +1,34 @@
+setTimeout(() => {
+
+
+
+
+
+}, 1000);
+
+// let ans = new Promise((res, rej) => {
+//   if (false) {
+//     return res()
+//   } else {
+//     return rej()
+//   }
+// })
+// ans
+//   .then(function() {
+//   alert("result")
+// })
+//   .catch(function() {
+//   alert("rejected")
+// })
+
+
+// let logo = document.querySelector('.logo')
+// async function a() {
+//   await fetch(settingsMenuBg);
+//   alert("got it");
+//
+//   logo.style.display = "none";
+
+
+// }
+// a()

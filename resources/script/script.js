3cf67305417ce2edaf414378185f53bfee93df1e
Mrinmoy <xmrinmoy@proton.me>
2023-08-16 19:39:48 +0530

    Add files via upload

---
+++ resources/script/script.js
@@ -0,0 +1,12 @@
+$(function(){
+  $(".headerx").load("../../assets/header.html"); 
+});
+$(function(){
+  $(".footerx").load("../../assets/footer.html"); 
+});
+$(function(){
+  $(".mobileNav").load("../../assets/mobileNav.html"); 
+});
+$(function(){
+  $(".settingsMenuBg").load("../../assets/settings.html"); 
+});

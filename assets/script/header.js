184a9659cffb1f185262c5be2a5a12cb24e0ebb8
Mrinmoy <xmrinmoy@proton.me>
2023-08-18 01:06:06 +0530

    Add files via upload

--- assets/script/header.js
+++ assets/script/header.js
@@ -1,10 +1,34 @@
 setTimeout(() => {
 
 
+  let deskSearch = document.querySelector('#deskSearch');
+  let deskFindBtn = document.querySelector('#deskFindBtn');
+  let deskSetBtn = document.querySelector('#deskSetBtn');
+  let deskFindVal = 0;
+
+
+
+    deskSearch.click();
+
+  deskFindBtn.addEventListener('click', ev => {
+    if (deskFindVal == 0) {   
+   deskSearch.style.cssText = "right: 3.25rem; opacity: 1;";
+    deskFindBtn.innerHTML = "<img src='./resources/images/cross.svg' id='findCloseBtn' class='iconx'>";
+      deskFindBtn.style.cssText = "transform: rotate(270deg); transition: 200ms ease-in-out;"
+      deskFindVal = 1;
+    } else { 
+   deskSearch.style.cssText = "right: -9.25rem;";
+    deskFindBtn.innerHTML = "<img src='./resources/images/search.svg' id='findCloseBtn' class='iconx'>";
+      deskFindBtn.style.cssText = "transform: rotate(360deg); transition: 200ms ease-in-out;"
+      deskFindVal = 0;
+    }
+  })
+
 
 
 
 }, 1000);
+
 
 // let ans = new Promise((res, rej) => {
 //   if (false) {

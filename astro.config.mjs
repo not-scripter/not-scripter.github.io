4a13d16cbc72bc181bce01b3cfae81e0b9763903
MRINMOY MONDAL <mm2922894@gmail.com>
2024-06-24 20:51:39 +0000

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,4 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [react(), tailwind()],
   site: "https://not-scripter.github.io",
-  base: "not-scripter.github.io",
 });

bcc4c4ac794dc28798e61bda1b8f73ac93b9e480
MRINMOY MONDAL <mm2922894@gmail.com>
2024-06-24 20:07:34 +0000

    added animation to hero section and fixed image path

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import { Spotlight } from "./ui/Spotlight";
 
 export default function Hero() {
@@ -7,13 +8,27 @@ export default function Hero() {
         className="-top-40 left-0 md:left-60 md:-top-20"
         fill="white"
       />
-      <div className="p-4 relative z-10  w-full text-center font-bold flex flex-col gap-2">
-        <h1 className="text-6xl bg-clip-text text-transparent bg-gradient-to-tr from-mauve to-lavender">
-          Namaste
-        </h1>
-        <img src="../../public/namaste.svg" alt="namaste" className="h-16" />
-        <p className="font-semibold">It's notscripter here</p>
-      </div>
+      <motion.h1
+        initial={{ opacity: 0.5, y: 100 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.3,
+          duration: 0.8,
+          ease: "easeInOut",
+        }}
+        className="bg-gradient-to-tr from-mauve to-lavender bg-clip-text text-center text-6xl font-bold tracking-tight text-transparent flex items-center flex-col justify-center gap-2"
+      >
+        Namaste
+        <img
+          src="https://not-scripter/not-scripter.github.io/public/namaste.svg"
+          alt="namaste"
+          className="h-16"
+        />
+        <span className="text-sm font-semibold tracking-normal">
+          It's notscripter here
+        </span>
+      </motion.h1>
     </div>
   );
 }
+// src="https://not-scripter/not-scripter.github.io/public/namaste.svg"
